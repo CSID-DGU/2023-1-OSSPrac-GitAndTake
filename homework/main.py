15c1bb989d9e46f7fcfb2115fefb9848117a7ee9
kim chang hyun <ckdgus9088@dongguk.edu>
2023-05-04 21:51:58 +0900

    feat main.py의 result() 함수 및 result.html의 Add_row(), Home() 함수 작 성

--- homework/main.py
+++ homework/main.py
@@ -25,3 +25,26 @@ def delete():
         new_data_list = [data for data in dataLIst if data['Student Number'] != studentNumber]
         dataLIst = new_data_list
     return render_template('result.html', result=dataLIst)
+
+# GET 데이터를 받아와서 처리하겠다
+# post 처리한 데이터를 포스팅(나열) 하겠다 
+@app.route('/result', methods=['GET','POST'])
+def result():
+    global dataLIst
+    if request.method == "POST":
+        # 딕셔너리 형태로 데이터 저장
+        data = {
+            "Name": request.form["Name"],
+            "Student Number": request.form["Student Number"],
+            "Major": request.form["Major"],
+            "Email": request.form["Email"] + "@" + request.form["Email_domain"],
+            "Gender": request.form.get("Gender"),
+            "Programing Language": ", ".join(request.form.getlist("Pro_lang[]"))
+        }
+        dataLIst.append(data)
+        dataLIst = sorted(dataLIst, key=lambda x: x.get('Student Number', '').lower())
+    return render_template('result.html', result=dataLIst)
+
+print('ex1.py is set to {}'.format(__name__))
+if __name__ =='__main__':
+    app.run(debug=True, port=8000)
