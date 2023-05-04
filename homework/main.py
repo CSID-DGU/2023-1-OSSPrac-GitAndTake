04aa6d0cab26d5d10936f714e7d3254706f7a8eb
chaehyunPark <allisha0508@naver.com>
2023-05-04 20:50:09 +0900

    feat: add main.py input(), home(), delete()

--- homework/main.py
+++ homework/main.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 
 app=Flask(__name__)
 app.secret_key = 'my_secret_key'
+dataLIst = []
 
 #mian page
 @app.route('/')
 def input():
     return render_template('main.html')
 
-#main page로 이동하되, 초기화 함
+#main page로 이동하되, 초기화 함    
 @app.route('/home')
 def home():
+    global dataLIst
+    dataLIst = []
+    return render_template('main.html')
+
 #check된 행 삭제
 @app.route('/delete', methods=['POST'])
 def delete():
-# GET 데이터를 받아와서 처리하겠다
-# post 처리한 데이터를 포스팅(나열) 하겠다 
-@app.route('/result', methods=['GET','POST'])
-def result():
-
-print('ex1.py is set to {}'.format(__name__))
-if __name__ =='__main__':
-    app.run(debug=True, port=8000)
+    global dataLIst
+    if request.method == "POST":
+        studentNumber = request.json['student_number']
+        new_data_list = [data for data in dataLIst if data['Student Number'] != studentNumber]
+        dataLIst = new_data_list
+    return render_template('result.html', result=dataLIst)
